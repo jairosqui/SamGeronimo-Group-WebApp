7c3ddde2a9de7f8210bd0756ff00f212ce2c709d
jairosqui <33742077+jairosqui@users.noreply.github.com>
2026-09-14 18:44:33 -0500

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -50,7 +50,7 @@ export function Hero() {
           className="relative aspect-[4/5] rounded-md2 overflow-hidden"
         >
           <Image
-            src="https://images.unsplash.com/photo-1758518729829-162d6bf27b5e?w=1600&q=70&auto=format&fit=crop"
+            src="https://unsplash.com/es/fotos/laptop-computer-on-glass-top-table-hpjSkU2UYSU"
             alt="Equipo empresarial diverso colaborando en una oficina moderna"
             fill
             priority
