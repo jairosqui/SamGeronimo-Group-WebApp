4ce0fe29728f22cf1bb03ee33fed9b07dd3d1a8b
jairosqui <33742077+jairosqui@users.noreply.github.com>
2026-09-14 19:21:34 -0500

    Update ContactForm.jsx

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import Script from "next/script";
 import { useLanguage } from "@/context/LanguageContext";
 
-const WEB3FORMS_ACCESS_KEY = "237bfbbb-39dc-4b99-825c-c4e109df365a";
+const WEB3FORMS_ACCESS_KEY = "672d2e56-41a6-490a-98d0-5185a51a44ae";
 const RECAPTCHA_SITE_KEY =
   process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || "YOUR_RECAPTCHA_SITE_KEY";
 
